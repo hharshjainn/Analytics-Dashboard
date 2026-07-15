002a6d41e680994dca312db652023afce5b8eb0d
Harsh Jain <hdoesntsharefood@gmail.com>
2026-07-15 23:30:08 +0530

    updated default state

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ st.set_page_config(
     page_title="Retention Analytics",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
 )
 
 
